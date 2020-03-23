fa69331b5eb44e5245932335956890363e786415
Mauricio Degregori <degregori2@gmail.com>
2020-03-23 15:20:57 -0500

    added pipfile

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,6 +2,15 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+#  * Should have the methods: `enqueue`, `dequeue`, and `len`.
+#    * `enqueue` should add an item to the back of the queue.
+#    * `dequeue` should remove and return an item from the front of the queue.
+#    * `len` returns the number of items in the queue.
+ 
+# ![Image of Queue](https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Data_Queue.svg/600px-Data_Queue.svg.png)
+
+#if doesn't work ask TL 
+
 
 class Queue:
     def __init__(self):
