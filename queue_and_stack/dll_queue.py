2b435f73d6c85d64ae4a23a16cc940e66a55bcee
Mauricio Degregori <degregori2@gmail.com>
2020-04-21 02:20:35 -0500

    finished queue_and_stack

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,18 +2,26 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
-
+#  * `enqueue` should add an item to the back of the queue.
+#    * `dequeue` should remove and return an item from the front of the queue.
+#    * `len` returns the number of items in the queue.
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_head(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return 
+        else:
+            self.size -= 1
+            removed = self.storage.remove_from_tail()
+            return removed
 
     def len(self):
-        pass
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,13 +6,21 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.storage.add_to_head(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        # pop() is an inbuilt function in Python that removes and returns last value from the list or the given index value.
+        # last value in stack would be head
+        if self.size == 0:
+            return 
+        else:
+            self.size -= 1
+            removed = self.storage.remove_from_head()
+            return removed
 
     def len(self):
-        pass
+        return self.size
